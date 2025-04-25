6b4051be2dae0162c916ddf031ebb95df52ee129
Justin Hassebroek <Justin.L.Hassebroek@gmail.com>
2025-04-25 13:48:52 -0600

    Updated code for how long the pump runs based on user input
    I had also included a flavor variable. These are hard coded until we figure out how we can update the variable with IoT. There is an issue with sometimes it shows that its pumping for 0 seconds and then repeats the message with the correct time of pumping, it doesn't delay the pump or make it stay on longer, just a bug to figure out.

--- Motors/Motors.ino
+++ Motors/Motors.ino
@@ -1,17 +1,29 @@
 int motor1 = 11;
+int pumpTime;
+String flavor1 = "grape";
 
 void setup() {
+  Serial.begin(9600);
   pinMode(motor1, OUTPUT);
 }
 
 void loop() {
+  Serial.print("How long would you like to mix in seconds: ");
+  delay(1000);
+
+  while(Serial.available() == 0){
+    //wait for input
+  }
+
+  pumpTime = Serial.parseInt();
+
+  Serial.println("\nYou are mixing " + flavor1 + " for " + String(pumpTime) + " seconds.\n");
+
   digitalWrite(motor1, HIGH);
   analogWrite(motor1, 255);
 
-  delay(2000);
+  delay(pumpTime * 1000);
   
   digitalWrite(motor1, HIGH);
   analogWrite(motor1, 0);
-
-  delay(2000);
 }

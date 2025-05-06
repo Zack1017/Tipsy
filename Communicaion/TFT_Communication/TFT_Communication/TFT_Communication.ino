975ef662b6aefca77c556cf165bdd4042db5aee6
Justin Hassebroek <Justin.L.Hassebroek@gmail.com>
2025-05-06 12:14:22 -0600

    Created TFT_Communication file
    Set up a file to test communication receives to the TFT slave from the Arduino master.

---
+++ Communicaion/TFT_Communication/TFT_Communication/TFT_Communication.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <Arduino.h>
+
+#define I2C_SLAVE_ADDR 0x3C
+#define SDA_PIN 8
+#define SCL_PIN 9
+#define BUFFER_SIZE 64
+
+byte receiveBuffer[BUFFER_SIZE];
+int receivedLength = 0;
+
+void receiveEvent(int numBytes) {
+  if (numBytes > BUFFER_SIZE) {
+    Serial.println("Write error: received more data than the buffer size");
+    return;
+  }
+
+  receivedLength = numBytes;
+  for (int i = 0; i < numBytes; i++) {
+    receiveBuffer[i] = Wire.read();
+  }
+
+  Serial.print("Received data: ");
+  for (int i = 0; i < receivedLength; i++) {
+    Serial.print(receiveBuffer[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void requestEvent() {
+  Wire.write(receiveBuffer, receivedLength); // Only send what was received
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); // Wait for Serial to be ready
+
+  Wire.begin(I2C_SLAVE_ADDR); // Start I2C in slave mode
+  Wire.onReceive(receiveEvent); // Correct function
+  Wire.onRequest(requestEvent); // Correct function
+
+  Serial.println("I2C Slave initialized");
+}
+
+void loop() {
+  delay(1000);
+}

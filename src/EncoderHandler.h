59abe45a5b35e80b8296c025c7a8f80414f535a8
Zackery Holloway <zackery.holloway@mines.sdsmt.edu>
2025-05-31 17:20:57 -0600

    Added Encoder Logic
    Added class base encoder set up, works with WireMangaer.

---
+++ src/EncoderHandler.h
@@ -0,0 +1,44 @@
+#ifndef ENCODER_HANDLER_H
+#define ENCODER_HANDLER_H
+
+#include <Adafruit_seesaw.h>
+
+#define SS_SWITCH_SELECT 1
+#define SS_SWITCH_UP     2
+#define SS_SWITCH_LEFT   3
+#define SS_SWITCH_DOWN   4
+#define SS_SWITCH_RIGHT  5
+
+#define SEESAW_ADDR      0x49
+
+enum ButtonIndex {
+  BTN_UP = 0,
+  BTN_DOWN,
+  BTN_LEFT,
+  BTN_RIGHT,
+  BTN_SELECT
+};
+
+class EncoderHandler{
+public:
+  EncoderHandler();
+  void begin();
+  void update();
+
+  int32_t getPosition();
+
+private:
+  static void interruptHandler(); //ISR
+  void handleInterrupt();
+
+  void checkEncoderChange();
+  void checkButtonPresses();
+
+  static EncoderHandler* instance; // For static ISR access
+  Adafruit_seesaw ss;
+  volatile int32_t encoder_position;
+  static constexpr unsigned long debounceDelay = 200; // milliseconds
+  unsigned long lastPressTime[5]; // One for each button
+};
+
+#endif
--- src/src.ino
+++ src/src.ino
@@ -1,8 +1,19 @@
-void setup() {
-  
+#include "EncoderHandler.h"
+#include "WireManager.h"
+
+EncoderHandler encoder;
+WireManager wire_;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(2000);
+  Serial.print("Looking for seesaw!");
+  wire_.begin();
+  encoder.begin();
 }
 
 void loop() 
 {
-  
+  encoder.update();
 }

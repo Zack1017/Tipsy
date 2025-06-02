9bb5bdc3f24dc180deec1000188cb475e926cfa0
Zackery Holloway <zackery.holloway@mines.sdsmt.edu>
2025-06-01 19:08:35 -0600

    Created Display Code

--- src_tft/DisplayManager.cpp
+++ src_tft/DisplayManager.cpp
@@ -1,57 +1,124 @@
 #include "DisplayManager.h"
 
-DisplayManager::DisplayManager() : displayed(false), colorWheel(nullptr) {
-    expander = new Arduino_XCA9554SWSPI(
-        PCA_TFT_RESET, PCA_TFT_CS, PCA_TFT_SCK, PCA_TFT_MOSI,
-        &Wire, 0x3F
-    );
+DisplayManager::DisplayManager() : colorWheel(nullptr), state(BOOTING), lastUpdateTime(0) {
+  expander = new Arduino_XCA9554SWSPI(
+    PCA_TFT_RESET, PCA_TFT_CS, PCA_TFT_SCK, PCA_TFT_MOSI,
+    &Wire, 0x3F
+  );
 
-    rgbpanel = new Arduino_ESP32RGBPanel(
-        TFT_DE, TFT_VSYNC, TFT_HSYNC, TFT_PCLK,
-        TFT_R1, TFT_R2, TFT_R3, TFT_R4, TFT_R5,
-        TFT_G0, TFT_G1, TFT_G2, TFT_G3, TFT_G4, TFT_G5,
-        TFT_B1, TFT_B2, TFT_B3, TFT_B4, TFT_B5,
-        1, 46, 2, 44,
-        1, 50, 16, 16
-    );
+  rgbpanel = new Arduino_ESP32RGBPanel(
+    TFT_DE, TFT_VSYNC, TFT_HSYNC, TFT_PCLK,
+    TFT_R1, TFT_R2, TFT_R3, TFT_R4, TFT_R5,
+    TFT_G0, TFT_G1, TFT_G2, TFT_G3, TFT_G4, TFT_G5,
+    TFT_B1, TFT_B2, TFT_B3, TFT_B4, TFT_B5,
+    1, 46, 2, 44,
+    1, 50, 16, 16
+  );
 
-    gfx = new Arduino_RGB_Display(
-        720, 720, rgbpanel, 0, true,
-        expander, GFX_NOT_DEFINED, hd40015c40_init_operations, sizeof(hd40015c40_init_operations)
-    );
+  gfx = new Arduino_RGB_Display(
+    720, 720, rgbpanel, 0, true,
+    expander, GFX_NOT_DEFINED, hd40015c40_init_operations, sizeof(hd40015c40_init_operations)
+  );
 }
 
-void DisplayManager::begin() {
-    Serial.begin(115200);
-    Wire.setClock(1000000);
 
-    Serial.println("Beginning");
 
-    if (!gfx->begin()) {
-        Serial.println("gfx->begin() failed!");
-        return;
-    }
+void DisplayManager::begin() 
+{
+  Serial.begin(115200);
+  Wire.setClock(1000000);
 
-    Serial.println("Initialized!");
+  Serial.println("Starting Tipsy...");
 
-    gfx->fillScreen(BLACK);
+  if (!gfx->begin()) 
+  {
+    Serial.println("gfx->begin() failed!");
+    return;
+  }
+  
+  gfx->fillScreen(BLACK);
+  expander->pinMode(PCA_TFT_BACKLIGHT, OUTPUT);
+  expander->digitalWrite(PCA_TFT_BACKLIGHT, HIGH);
 
-    expander->pinMode(PCA_TFT_BACKLIGHT, OUTPUT);
-    expander->digitalWrite(PCA_TFT_BACKLIGHT, HIGH);
-
-    colorWheel = (uint16_t *)ps_malloc(gfx->width() * gfx->height() * sizeof(uint16_t));
-    if (colorWheel) {
-        generateColorWheel();
-        gfx->draw16bitRGBBitmap(0, 0, colorWheel, gfx->width(), gfx->height());
-    }
+  state = BOOTING;
+  lastDrawnState = READY;
+  lastUpdateTime = millis();
 }
+
+
 
 void DisplayManager::update() {
-    if (!displayed) {
-        drawText();
-        displayed = true;
+    unsigned long now = millis();
+
+    switch (state) {
+        case BOOTING:
+            if (lastDrawnState != BOOTING) {
+                drawBootScreen();
+                lastDrawnState = BOOTING;
+            }
+            if (now - lastUpdateTime > 4000) {
+                state = WELCOME;
+                lastUpdateTime = now;
+            }
+            break;
+
+        case WELCOME:
+            if (lastDrawnState != WELCOME) {
+                drawWelcomeScreen();
+                lastDrawnState = WELCOME;
+            }
+            if (now - lastUpdateTime > 15000) {
+                state = READY;
+                lastUpdateTime = now;
+            }
+            break;
+
+        case READY:
+            if (lastDrawnState != READY) {
+                gfx->fillScreen(BLACK);
+                gfx->setCursor(100, 100);
+                gfx->setTextColor(WHITE);
+                gfx->setTextSize(3);
+                gfx->println("Ready for action!");
+                lastDrawnState = READY;
+            }
+            break;
     }
 }
+
+
+
+void DisplayManager::drawBootScreen()
+{
+  gfx->fillScreen(BLACK);
+  gfx->setCursor(120, gfx->height()/2 -20);
+  gfx->setTextSize(3);
+  gfx->setTextColor(WHITE);
+  gfx->println("Booting...");
+}
+
+
+
+void DisplayManager::drawWelcomeScreen() 
+{
+  gfx->fillScreen(BLACK);
+  gfx->setCursor(150, gfx->height() / 2 - 80);
+  gfx->setTextSize(8);
+  gfx->setTextColor(WHITE);
+  gfx->println("Welcome to");
+
+  gfx->setCursor(200, gfx->height() / 2);
+  gfx->setTextSize(10);
+  gfx->setTextColor(OLIVE);
+  gfx->println("Tipsy!");
+
+  gfx->setCursor(150, gfx->height() / 2 + 100);
+  gfx->setTextSize(4);
+  gfx->setTextColor(LIGHTGREY);
+  gfx->println("Your robot bartender");
+}
+
+
 
 void DisplayManager::generateColorWheel() {
     int width = gfx->width();
@@ -79,23 +146,4 @@ void DisplayManager::generateColorWheel() {
             }
         }
     }
-}
-
-void DisplayManager::drawText() {
-    gfx->setCursor(100, gfx->height() / 2 - 75);
-    gfx->setTextSize(5);
-    gfx->setTextColor(WHITE);
-    gfx->println("Hello World!");
-
-    gfx->setCursor(100, gfx->height() / 2 - 25);
-    gfx->setTextColor(YELLOW);
-    gfx->println("YELLOW");
-
-    gfx->setCursor(100, gfx->height() / 2 + 25);
-    gfx->setTextColor(GREEN);
-    gfx->println("GREEN");
-
-    gfx->setCursor(100, gfx->height() / 2 + 75);
-    gfx->setTextColor(BLUE);
-    gfx->println("BLUE");
 }
--- src_tft/DisplayManager.h
+++ src_tft/DisplayManager.h
@@ -3,22 +3,32 @@
 
 #include <Arduino_GFX_Library.h>
 
+enum DisplayState{
+  BOOTING,
+  WELCOME,
+  READY
+};
+
 class DisplayManager {
 public:
-    DisplayManager();
-    void begin();
-    void update();
+  DisplayManager();
+  void begin();
+  void update();
 
 private:
-    void generateColorWheel();
-    void drawText();
+  void generateColorWheel();
+  void drawBootScreen();
+  void drawWelcomeScreen();
 
-    Arduino_XCA9554SWSPI *expander;
-    Arduino_ESP32RGBPanel *rgbpanel;
-    Arduino_RGB_Display *gfx;
+  Arduino_XCA9554SWSPI *expander;
+  Arduino_ESP32RGBPanel *rgbpanel;
+  Arduino_RGB_Display *gfx;
 
-    uint16_t *colorWheel;
-    bool displayed;
+  uint16_t *colorWheel;
+  DisplayState state;
+  DisplayState lastDrawnState;
+  unsigned long lastUpdateTime;
+  
 };
 
 #endif
